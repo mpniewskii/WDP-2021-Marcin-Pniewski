a76113a72465dca5678762b70f03447efb367c9b
mpniewskii <93130304+mpniewskii@users.noreply.github.com>
2021-11-08 14:26:49 +0100

    Update zad 12.py

--- Lab 05/zad 12.py
+++ Lab 05/zad 12.py
@@ -1,9 +1,16 @@
 labirynt = [[1, 1, 1, 1, 1, 1, 1], [1, 2, 1, 0, 0, 0, 1], [1, 0, 1, 0, 1, 1, 1], [1, 0, 0, 0, 0, 0, 1], [1, 0, 1, 1, 1, 0, 1], [1, 0, 0, 0, 1, 3, 1], [1, 1, 1, 1, 1, 1, 1]]
 ruchy = 0
+x = 1
+y = 1
+
 for i in labirynt:
     print(i)
 
 while ruchy<5:
     print("Podaj ruch")
     tempruch = input()
-    if tempruch = 'w'
+    if tempruch == 'w':
+        labirynt[x][y] = 0
+        labirynt[x+1][y] = 2
+        for i in labirynt:
+            print(i)
