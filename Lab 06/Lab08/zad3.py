72a28aea6f8a61f76ef46e9c68d5ec7e806c9e92
mpniewskii <93130304+mpniewskii@users.noreply.github.com>
2021-12-12 21:03:17 +0100

    Delete Lab 06/Lab08 directory

--- Lab 06/Lab08/zad3.py
+++
@@ -1,7 +0,0 @@
-def kafle(x, y):
-    if x == 1:
-        return y
-    else:
-        return x*x*kafle(1, y)
-
-print(kafle(3, 6))
--- Lab 06/Lab08/zad4.py
+++
@@ -1,9 +0,0 @@
-def binarka(n):
-    if n==1:
-        return 1
-    else:
-        return str(binarka(n//2)) + str(n%2)
-
-
-
-print(binarka(2))
