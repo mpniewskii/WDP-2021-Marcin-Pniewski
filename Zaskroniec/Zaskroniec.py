b367ecf0b792a6e44239646e7cc31651528636dc
mpniewskii <93130304+mpniewskii@users.noreply.github.com>
2022-01-17 13:21:01 +0100

    Update Zaskroniec.py

--- Zaskroniec/Zaskroniec.py
+++ Zaskroniec/Zaskroniec.py
@@ -16,18 +16,32 @@ def uruchom():
     dolsciana = pygame.draw.rect(SCREEN, (255, 0, 0), (0, 790, 890, 10))
     lewasciana = pygame.draw.rect(SCREEN, (255, 0, 0), (0, 0, 10, 890))
     prawasciana = pygame.draw.rect(SCREEN, (255, 0, 0), (890, 0, 10, 890))
-    jedzenie1 = pygame.draw.rect(SCREEN, KOLOREKJEDZ, [Xjedzeniaupdate, Yjedzeniaupdate, 20, 20])
+    jedzenie1 = SCREEN.blit(zwykle_jedzenie, (Xjedzeniaupdate, Yjedzeniaupdate))
+
 
     mixer.init()
     przegrales = False
 
 
 def game_over():
+    with open('High_Score.txt', 'r', encoding='utf-8') as file:
+        high_score = file.read()
+        high_score = int(high_score)
+        if punkty>high_score:
+            high_score=punkty;
+    with open('High_Score.txt', 'w', encoding='utf-8') as file:
+        if punkty >= high_score:
+            file.write(f'{punkty}')
+        else:
+            file.write(f'{high_score}')
+
     SCREEN.blit(cmentarz, [0, 0])
     napis_game_over = czcionka_game_over.render("Przegrałeś! Twoja punktacja to: " + str(punkty), 1, (0, 0, 0))
+    napis_high_score = czcionka_game_over.render("Twój high score to: " + str(high_score) + " całe te", 1, (0, 0, 0))
     instrukcje = czcionka_game_over.render("Naciśnij Y by wyjść!", 1, (0, 0, 0))
     SCREEN.blit(napis_game_over, (130, 10))
-    SCREEN.blit(instrukcje, (130, 110))
+    SCREEN.blit(instrukcje, (140, 110))
+    SCREEN.blit(napis_high_score, (130, 190))
     kordy.clear()
     mixer.music.load('game_over_soundtrack.mp3')
     mixer.music.play()
@@ -81,6 +95,9 @@ FPSy = pygame.time.Clock()
 
 essa = pygame.image.load('Tlo.jpg').convert_alpha()
 essa = pygame.transform.scale(essa, (900, 800))
+
+zwykle_jedzenie = pygame.image.load('zwykle_jedzenie.jpg').convert_alpha()
+zwykle_jedzenie = pygame.transform.scale(zwykle_jedzenie, (20, 20))
 
 czcionka = pygame.font.SysFont("monospace", 12)
 czcionka_game_over = pygame.font.SysFont("arial", 22)
@@ -139,6 +156,9 @@ while is_running:
             pygame.mixer.Channel(1).play(pygame.mixer.Sound('jedzenie.mp3'))
             Xjedzeniaupdate = int(random.randint(30,850))
             Yjedzeniaupdate = int(random.randint(30,750))
+            if Xjedzeniaupdate in kordy or Yjedzeniaupdate in kordy:
+                Xjedzeniaupdate = int(random.randint(30, 850))
+                Yjedzeniaupdate = int(random.randint(30, 750))
             dlugosc += 1
             szybkosc += 0.1
             punkty += 1
@@ -150,6 +170,9 @@ while is_running:
             pygame.mixer.Channel(1).play(pygame.mixer.Sound('jedzenie.mp3'))
             Xmleczka = int(random.randint(30,850))
             Ymleczka = int(random.randint(30,750))
+            if Xmleczka in kordy or Ymleczka in kordy:
+                Xmleczka = int(random.randint(30, 850))
+                Ymleczka = int(random.randint(30, 750))
             szybkosc -= 0.5
             SCREEN.blit(mleczko_power_up, (Xmleczka, Ymleczka))
             szansa_power_up = int(random.randint(0, 1))
@@ -157,9 +180,12 @@ while is_running:
                 ktory_power_up = int(random.randint(0, 1))
 
         if ((Xzupki + 20 > Xglowy and Xzupki - 20 < Xglowy) and (Yzupki + 20 > Yglowy and Yzupki - 20 < Yglowy)):
-            pygame.mixer.Channel(1).play(pygame.mixer.Sound('jedzenie.mp3'))
+            pygame.mixer.Channel(1).play(pygame.mixer.Sound('major-smiechu-warte.mp3'))
             Xzupki = int(random.randint(30,850))
             Yzupki = int(random.randint(30,750))
+            if Xzupki in kordy or Yzupki in kordy:
+                Xzupki = int(random.randint(30, 850))
+                Yzupki = int(random.randint(30, 750))
             szybkosc += 0.5
             dlugosc -= 1
             del kordy[0]
@@ -182,7 +208,7 @@ while is_running:
                 przegrales = True
         if Yglowy > 770 or Yglowy < 10 or Xglowy > 870 or Xglowy < 10:
             przegrales = True
-        if przegrales == True:
+        if przegrales:
             game_over()
 
 
@@ -207,3 +233,4 @@ pygame.quit()
 
 #Pracowalem_na_zajeciach_10.01_zdalnie_z_powodu_slabego_samopoczucia
 #Dodaje_pogram_przepisany_na_funkcje
+#Dodalem_high_scores_i_nowe_dzwieki(17.01)
